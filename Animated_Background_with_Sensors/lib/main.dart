d386e86b16695b37a2e4189ceccabcf4a7888080
remon <blackshadowsoftwareltd@gmail.com>
2022-07-01 23:42:50 +0600

    animated positioned

--- Animated_Background_with_Sensors/lib/main.dart
+++ Animated_Background_with_Sensors/lib/main.dart
@@ -18,10 +18,10 @@ class _HomeState extends State<Home> {
     gyroscopeEvents.listen(
       (GyroscopeEvent event) {
         if (event.y.abs() > 0.0) {
-          initX = initX + (event.y * 2);
+          initX = initX + (event.y);
         }
         if (event.x.abs() > 0.0) {
-          initY = initY + (event.x * 2);
+          initY = initY + (event.x);
         }
 
         setState(() {});
@@ -33,35 +33,40 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: StreamBuilder<GyroscopeEvent>(
-        stream: SensorsPlatform.instance.gyroscopeEvents,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data!.y.abs() > 0.0) {
-              initX = initX + (snapshot.data!.y * 2);
-            }
-            if (snapshot.data!.x.abs() > 0.0) {
-              initY = initY + (snapshot.data!.x * 2);
-            }
-          }
+        body:
+            // StreamBuilder<GyroscopeEvent>(
+            //   stream: SensorsPlatform.instance.gyroscopeEvents,
+            //   builder: (context, snapshot) {
+            //     if (snapshot.hasData) {
+            //       if (snapshot.data!.y.abs() > 0.0) {
+            //         initX = initX + (snapshot.data!.y * 2);
+            //       }
+            //       if (snapshot.data!.x.abs() > 0.0) {
+            //         initY = initY + (snapshot.data!.x * 2);
+            //       }
+            //     }
 
-          return Stack(
-            children: [
-              Positioned(
-                  left: -20 - initX,
-                  right: -20 + initX,
-                  top: -20 - initY,
-                  bottom: -20 + initY,
-                  child: SizedBox(
-                      width: size.width,
-                      height: size.height,
-                      child: Image.network(
-                          'https://images.unsplash.com/photo-1536223018115-f34afa40e19d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
-                          fit: BoxFit.cover))),
-            ],
-          );
-        },
-      ),
-    );
+            //     return
+            Stack(
+      children: [
+        AnimatedPositioned(
+            left: -20 - initX,
+            right: -20 + initX,
+            top: -20 - initY,
+            bottom: -20 + initY,
+            curve: Curves.easeIn,
+            duration: const Duration(milliseconds: 200),
+            child: SizedBox(
+                width: size.width,
+                height: size.height,
+                child: Image.network(
+                    'https://images.unsplash.com/photo-1536223018115-f34afa40e19d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+                    fit: BoxFit.cover))),
+      ],
+    )
+        // ;
+        //     },
+        // ),
+        );
   }
 }
